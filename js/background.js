9c1e0638e8e95916faf7dd0f89e74c555aa94552
AaronPlave <aplave@wesleyan.edu>
2014-03-16 03:25:10 +0100

    Locations now grab wikipedia data and thumbnail and display on the map, not styled yet

--- js/background.js
+++ js/background.js
@@ -56,6 +56,17 @@ function updateLocation(location, coords) {
             y: coords.A
         };
         console.log(coords);
+
+
+        //async get data but don't wait around for this to happen,
+        // just push another locations update on return of getWiki.
+        getWiki(location, function() {
+            chrome.runtime.sendMessage({
+                action: 'update_locations',
+                updatedLocations: locations
+            })
+        })
+
         //push locations out to whoever is listening-- i.e. 
         //content script and popup
         chrome.runtime.sendMessage({
@@ -101,7 +112,7 @@ function geolocate(address, callback) {
         'address': address
     }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results[0]);
+            console.log(results);
             coords = results[0].geometry.location;
             console.log("HERE THEY ARE", coords);
             callback(address, coords);
@@ -119,9 +130,121 @@ function getLocationData() {
     return locationData
 }
 
+
+function getWikiImage(loc, title,callback) {
+    //return a summary and image of the location from wiki
+    //right now this takes two querries, maybe find a way to reduce to only 1
+    // Question: do this immediately or only when the user selects "read more"
+    // on the map page.
+
+    var wikiImageQueryBase = "http://en.wikipedia.org/w/api.php?action=query&titles=QUERY_STRING&prop=pageimages&format=json&pithumbsize=500"
+    wikiImageQueryBase = wikiImageQueryBase.replace("QUERY_STRING", loc);
+
+
+    console.log(loc);
+    console.log(title);
+    $.get(
+        wikiImageQueryBase,
+        function(data) {
+            console.log(data.query.search)
+            var pageKey = Object.keys(data.query.pages)
+            if (!(pageKey)) {
+                console.log("NO IMAGE PAGE RESULTS");
+                return callback();
+            }
+            // console.log(pageKey)
+            // console.log(response.responseJSON.query.pages[pageKey])
+            var imageUrl;
+            if (data.query.pages[pageKey].thumbnail.source) {
+                imageUrl = data.query.pages[pageKey].thumbnail.source;
+            }
+            console.log(imageUrl);
+
+            // now set extract of loc
+            if (loc in locations) {
+                l = locations[loc];
+                l.imageUrl = imageUrl;
+            }
+            return callback()
+        }
+    );
+}
+
+
+
+function getWikiSummary(loc, url, callback) {
+    console.log(url);
+    var wikiExtractQueryBase = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles=QUERY_STRING&format=json&inprop=url";
+    wikiExtractQueryBase = wikiExtractQueryBase.replace("QUERY_STRING", loc);
+    $.get(
+        wikiExtractQueryBase,
+        function(data) {
+            console.log(data.query.search)
+            var pageKey = Object.keys(data.query.pages)
+            if (!(pageKey)) {
+                console.log("NO SEARCH RESULTS");
+                return callback();
+            }
+            // console.log(pageKey)
+            // console.log(response.responseJSON.query.pages[pageKey])
+            var extract = data.query.pages[pageKey].extract;
+            var title = data.query.pages[pageKey].title;
+            console.log(extract);
+
+            // now set extract of loc
+            if (loc in locations) {
+                l = locations[loc];
+                l.extract = extract;
+            }
+            return getWikiImage(loc, title, callback);
+        }
+    );
+}
+
+
+//maaaaybe do this. Search wiki for the first result of location, use that to get image and extract from result title.
+
+
+// can also use this for images -- 
+// http://en.wikipedia.org/w/api.php?action=query&titles=QUERY_STRING&prop=pageimages&format=json&pithumbsize=500
+
+function getWiki(loc, callback) {
+
+    //first query WIKI api for url of corrosponding article to location
+    var wikiSearchQueryBase = "http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=QUERY_STRING&srlimit=1"
+    wikiSearchQueryBase = wikiSearchQueryBase.replace("QUERY_STRING", loc);
+    // console.log(wikiQueryBase, loc)
+    var response;
+    $.get(
+        wikiSearchQueryBase,
+        function(data) {
+
+            console.log(data.query.search)
+            var results = data.query.search;
+            if (!(results)) {
+                console.log("NO SEARCH RESULTS");
+                return callback();
+            }
+            // console.log(pageKey)
+            // console.log(response.responseJSON.query.pages[pageKey])
+            var title = results[0].title;
+            console.log(title);
+            return getWikiSummary(loc, "http://en.wikipedia.org/wiki/" + title, callback);
+        }
+    );
+
+
+
+
+}
+
 //REMEMBER to escape locations on the front end
 
 //might want to add callback for addLocation since async, but should be alright...?
+
+// http://en.wikipedia.org/wiki/Special%3aApiSandbox#action=query&prop=extracts&format=json&exlimit=10&exintro=&titles=London
+// ^ wiki api link to get paragraph of info on a query
+
 
 // Handle request to add location
 //TODO: could change to switch statements later?
@@ -129,7 +252,6 @@ function getLocationData() {
 // coordinates? Worth the wait for the popup though..? 
 function onRequest(request, sender, sendResponse) {
     console.log(request, sender.tab, request.action);
-    //DO I EVER USE THIS?
     if (request.action == "add_location") {
         addLocation(request.newLocation);
         // sendResponse({
--- js/map.js
+++ js/map.js
@@ -100,12 +100,51 @@ function initialize() {
     //TODO: Center at current location
     var mapOptions = {
         zoom: 5,
+        minZoom: 2,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         styles: blueWater,
         center: new google.maps.LatLng(-34.397, 150.644)
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
+    // google.maps.event.addListener(map, 'center_changed', function() {
+    //     checkBounds();
+    // });
+    // boundary fix
+
+    // var allowedBounds = new google.maps.LatLngBounds(
+    //     new google.maps.LatLng(80, -160), // top left corner of map
+    //     new google.maps.LatLng(-80, 160) // bottom right corner
+    // );
+
+    // function checkBounds() {
+    //     if (!allowedBounds.contains(map.getCenter())) {
+    //         var C = map.getCenter();
+    //         var X = C.lng();
+    //         var Y = C.lat();
+
+    //         var AmaxX = allowedBounds.getNorthEast().lng();
+    //         var AmaxY = allowedBounds.getNorthEast().lat();
+    //         var AminX = allowedBounds.getSouthWest().lng();
+    //         var AminY = allowedBounds.getSouthWest().lat();
+
+    //         if (X < AminX) {
+    //             X = AminX;
+    //         }
+    //         if (X > AmaxX) {
+    //             X = AmaxX;
+    //         }
+    //         if (Y < AminY) {
+    //             Y = AminY;
+    //         }
+    //         if (Y > AmaxY) {
+    //             Y = AmaxY;
+    //         }
+
+    //         map.setCenter(new google.maps.LatLng(Y, X));
+    //     }
+    // }
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
@@ -132,9 +171,34 @@ function getLocationData() {
 // Maybe have a load-2+-pts-to-GMaps
 
 // TODO: implement method to remove locations onClick
-function removeLocation(){};
+function removeLocation() {};
 
-function onRowClick(){};
+
+//wire up listener for locations in rows
+function setRowListeners() {
+    //maybe set a hover listener to make
+    //all other locs opaque when any row
+    //is being hovered over
+    console.log("setting")
+    rowLocations = $(".rowLocation");
+    console.log(rowLocations)
+    rowLocations.on('click', function() {
+        onRowClick(this.id)
+    })
+
+    //on click of a location 
+}
+
+function onRowClick(loc) {
+    console.log(loc, 1)
+    if (loc in markers) {
+        m = markers[loc];
+        map.panTo(m.getPosition());
+        map.setZoom(9)
+    }
+};
+
+
 // moves to position on map, maybe changes the color, 
 // maybe pops up more info, maybe calc distances btwn 
 // every location and the selected one 
@@ -148,10 +212,10 @@ function displayLocations(locationData) {
     //{locations:[{location:{x:float,y:float},...]}
     obj = JSON.parse(locationData);
     addMarkersToMap(obj);
-    addMarkersToList(obj);
+    addLocationsToTable(obj);
 }
 
-var markers = [];
+var markers = {};
 
 function addMarkersToMap(locations) {
 
@@ -161,11 +225,13 @@ function addMarkersToMap(locations) {
     // properly formatted/exist for each obj tried
 
     for (loc in locations) {
+        console.log(locations[loc]);
         //TODO: Add clustering of markers
         //TODO!! Could pull some wikipedia or google data 
         //for each location on the map. For now just display and label
         var contentString = '<div id="content">' + loc +
-            '</div>';
+            '</div>'+'<img src="'+locations[loc].imageUrl+'">'+
+            '<div id="locDescription"'+locations[loc].extract+'"/>';
         var infowindow = new google.maps.InfoWindow({
             content: contentString
         });
@@ -183,23 +249,21 @@ function addMarkersToMap(locations) {
                 };
             }(infowindow, marker)
         );
-        markers.push(marker);
+        markers[loc] = marker;
 
         bounds.extend(coords);
         map.fitBounds(bounds);
     }
 }
 
-function addMarkersToList(locations) {
+function addLocationsToTable(locations) {
     locList = $("#locationTable");
     for (loc in locations) {
-        var contentString = 
-        '<tr>' +
-          '<td>'+loc+'</td>' + 
-          '<td>'+locations[loc].x+' '+locations[loc].y+'</td>' +
-        '</tr>'
-
-        console.log(contentString,"1")
+        var contentString =
+            '<tr>' +
+            '<td class="rowLocation" id="' + loc + '"">' + loc + '</td>' +
+            '<td>' + locations[loc].x + ' ' + locations[loc].y + '</td>' +
+            '</tr>'
         locList.append(contentString);
     }
 
@@ -212,6 +276,7 @@ function addMarkersToList(locations) {
         clean: true,
         cleanElements: "th td",
     });
+    setRowListeners();
 }
 
 
